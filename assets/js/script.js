e735cb6c896b538a4af27ce65bff057fc01e70a0
induwara-IIT <induwara.20230163@iit.ac.lk>
2024-11-25 00:30:39 +0530

    FAQ section
    Added the FAQ section, changed back to top button, Linked navigation buttons, added GitHub icon to relevant places.

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,23 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+
+
+var acc = document.getElementsByClassName("accordion");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+          this.classList.toggle("active");
+          this.parentElement.classList.toggle("active");
+
+          var pannel = this.nextElementSibling;
+
+          if (pannel.style.display === "block") {
+            pannel.style.display = "none";
+          } else {
+            pannel.style.display = "block";
+          }
+        });
+      }
